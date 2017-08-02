176ccfa5c397ecad2e4c1545e1f657257aad74ac
yuanshuai <1291781853@qq.com>
2017-08-02 16:20:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yuanshuai/find/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.yuanshuai.find.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yuanshuai.find.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
