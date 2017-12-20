c944428a148006557350cc93d3ed84e33ad40f59
yuanshuai <1291781853@qq.com>
2017-12-20 21:44:11 +0800

    frist

--- app/src/main/java/com/example/yuanshuai/find/activity/Register.java
+++ app/src/main/java/com/example/yuanshuai/find/activity/Register.java
@@ -7,28 +7,42 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.yuanshuai.find.R;
+import com.example.yuanshuai.find.net.Net;
 import com.example.yuanshuai.find.tool.CheckPhone;
+import com.example.yuanshuai.find.tool.CoutDownTimerUtils;
+import com.jakewharton.rxbinding.view.RxView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class Register extends AppCompatActivity {
-    @BindView(R.id.user)
-    EditText user;
+    @BindView(R.id.number)
+    EditText number;
     @BindView(R.id.password)
     EditText password;
     @BindView(R.id.confirm)
     EditText confirm;
     @BindView(R.id.reg)
     Button reg;
+    @BindView(R.id.getcode)
+    TextView getcode;
+
+
+
+    private CoutDownTimerUtils coutDownTimerUtils;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         ButterKnife.bind(this);
+        init();
+//        initEvent();
     }
     public void showSnackBar(String message){
         final Snackbar snackbar=Snackbar.make(getWindow().getDecorView(),message,Snackbar.LENGTH_INDEFINITE);
@@ -40,21 +54,21 @@ public class Register extends AppCompatActivity {
         });
         snackbar.show();
     }
-    private void reg(){
-        String number=user.getText().toString().trim();
-        String pwd=password.getText().toString().trim();
-        String pwdconfirm=confirm.getText().toString().trim();
-        if(!CheckPhone.isPhone(number))
-        {
-            user.setError("请输入正确的手机号");
-        }
-        else if(!pwd.equals(pwdconfirm)) {
-            confirm.setError("密码不一致");
-        }
-        else{
-            register(number,pwd);
-        }
-    }
+//    private void reg(){
+//        String number=number.getText().toString().trim();
+//        String pwd=password.getText().toString().trim();
+//        String pwdconfirm=confirm.getText().toString().trim();
+//        if(!CheckPhone.isPhone(number))
+//        {
+//            user.setError("请输入正确的手机号");
+//        }
+//        else if(!pwd.equals(pwdconfirm)) {
+//            confirm.setError("密码不一致");
+//        }
+//        else{
+//            register(number,pwd);
+//        }
+//    }
     private void register(String name,String password){
 
     }
@@ -63,4 +77,32 @@ public class Register extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
     }
+
+    private  void init(){
+        coutDownTimerUtils=new CoutDownTimerUtils(getcode,60000,1000);
+    }
+
+//    private void initEvent(){
+//        RxView.clicks(getcode).subscribe(new Action1<Void>() {
+//            @Override
+//            public void call(Void aVoid) {
+//                if(confirmNumber()){
+//                    Net.getNet().getcode(number.getText().toString())
+//                            .subscribeOn(Schedulers.io())
+//                            .observeOn(Schedulers.immediate())
+//                            .subscribe(new Action1<Response>() {
+//                                @Override
+//                                public void call(Response response) {
+//                                    if (response.getResult().equals("发送成功"))
+//                                        coutDownTimerUtilsj.start();
+//
+//                                }
+//                            }, new Action1<Throwable>() {
+//                                @Override
+//                                public void call(Throwable throwable) {
+//                                    showSnackBar(throwable.getMessage());
+//                                }
+//                            });
+//                }
+//            }}
 }
